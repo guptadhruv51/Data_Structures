989c327b053c4bc0d8104f412e352b62893a76ac
Dhruv Gupta <guptadhruv51@gmail.com>
2024-05-21 08:54:09 +1000

    Arguments and Rest parameter added

--- JavaScript/functions-Basic-advanced/arguments.js
+++ JavaScript/functions-Basic-advanced/arguments.js
@@ -16,8 +16,9 @@ calculateTotal(1,2,3,4,5);
 
 function calculateTotal2(a,b,...rest)
 {
+  //rest: should be the last in the parameter list
 //a=4,b=5 rest=[7,8,9,1,2,3]
 console.log(a,b);
-console.log(rest);
+console.log(rest); //array: can do the array operations
 }
 calculateTotal2(4,5,6,7,8,9,1,2,3)
