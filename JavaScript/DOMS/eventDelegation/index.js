27b134390a6a427934905a1a9aeac01af854f6f5
Dhruv Gupta <guptadhruv51@gmail.com>
2025-04-22 13:34:04 +1000

    Added Meme Generator, and understood DOM

---
+++ JavaScript/DOMS/eventDelegation/index.js
@@ -0,0 +1,9 @@
+let btnContainer=document.querySelector("#btn-container");
+
+btnContainer.addEventListener("click",(event)=>
+{
+  // console.log(event);
+  // console.log(event.target);
+  
+
+})
--- JavaScript/DOMS/index.js
+++ JavaScript/DOMS/index.js
@@ -1,6 +1,54 @@
 let startBtn=document.querySelector("#btn-start");
+let cntBtn=document.querySelector("#btn-count");
+let stopBtn=document.querySelector("#btn-stop");
+let value=0;
+let input=document.querySelector(".input");
+let enterBtn=document.querySelector("#btn-enter");
+// input.addEventListener("change",()=>
+// {
+//   console.log(input.value);
+// })
+enterBtn.addEventListener("click",()=>
+{
+  console.log(input.value);
+})
+
+// input.addEventListener("input",()=>
+// {
+//   console.log(input.value);
+// })
+
 function showMessage()
 {
   console.log("Button CLicked");
 }
-startBtn.addEventListener("click",showMessage);
+startBtn.addEventListener("click",()=>
+  {
+    // if(startBtn.innerText==="Start")
+    // {
+    //   startBtn.innerText="Clicked";
+    // }
+    // else
+    // {
+    //   startBtn.innerText="Start";
+    // }
+
+    startBtn.innerHTML="<span> Clicked </span>"
+});
+
+cntBtn.addEventListener("click",()=>
+{
+  value++;
+    console.log(value);
+})
+
+stopBtn.addEventListener("click",()=>
+{   
+  // if(!stopBtn.classList.contains("btn-red"))
+  //   stopBtn.classList.add("btn-red");
+  // else{
+  //   stopBtn.classList.remove("btn-red");
+  // }
+
+  stopBtn.classList.toggle("btn-red");
+})
